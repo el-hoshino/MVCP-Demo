b374dda51144cf360f17d507b679beebf872d67d
星野恵瑠 <shonetesmith@gmail.com>
2017-02-18 12:19:19 +0900

    Declare  first controller

---
+++ MVCP-Demo/MVCP-Demo/Controllers/FirstController.swift
@@ -0,0 +1,65 @@
+//
+//  FirstController.swift
+//  MVCP-Demo
+//
+//  Created by 史翔新 on 2017/02/18.
+//  Copyright © 2017年 crazism.net. All rights reserved.
+//
+
+import UIKit
+
+class FirstController: UIViewController {
+	
+	weak var presenter: Presenter?
+	var tappedLocation: CGPoint?
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		self.reset()
+	}
+	
+	override func viewDidLoad() {
+		self.setGestureRecognizers()
+	}
+	
+}
+
+extension FirstController {
+	
+	fileprivate func setGestureRecognizers() {
+		
+		let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTap(sender:)))
+		self.view.addGestureRecognizer(tapRecognizer)
+		
+	}
+	
+}
+
+extension FirstController {
+	
+	fileprivate func reset() {
+		self.tappedLocation = nil
+	}
+	
+}
+
+extension FirstController {
+	
+	@objc fileprivate func didTap(sender: UITapGestureRecognizer) {
+		self.setLocation(from: sender)
+		self.presentNextController()
+	}
+	
+	private func setLocation(from sender: UITapGestureRecognizer) {
+		self.tappedLocation = sender.location(in: self.view)
+	}
+	
+	private func presentNextController() {
+		
+		if let next = self.presenter?.nextPresentation(after: self, userInfo: nil) {
+			self.present(next.controller, animated: true, completion: next.completion)
+		}
+		
+	}
+	
+}
